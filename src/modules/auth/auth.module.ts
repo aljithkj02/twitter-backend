a4ee63caccc4065fcb2b86993780557dd01ddf91
aljithkj02 <kwdeathlok@gmail.com>
2024-09-03 15:03:06 +0530

    Catch firebase errors

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -3,10 +3,11 @@ import { AuthService } from '@modules/auth/auth.service';
 import { AuthController } from '@modules/auth/auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from '@modules/user/entities/user.entity';
+import { FirebaseClientService } from '@modules/firebase/firebase-client.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, FirebaseClientService],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -10,12 +10,15 @@ import { Repository } from 'typeorm';
 import { RegisterUserDto } from '@modules/auth/dto/register-user.dto';
 import * as FirebaseAuth from 'firebase/auth';
 import { LoginUserDto } from '@modules/auth/dto/login-user.dto';
+import { FirebaseClientService } from '@modules/firebase/firebase-client.service';
+import { FirebaseError } from 'firebase/app';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
+    private readonly firebaseClientService: FirebaseClientService,
   ) {}
 
   async findAll() {
@@ -23,54 +26,76 @@ export class AuthService {
   }
 
   async registerUser({ name, email, password }: RegisterUserDto) {
-    const isUserExist = await this.userRepository.findOneBy({
-      email,
-    });
+    try {
+      const isUserExist = await this.userRepository.findOneBy({
+        email,
+      });
 
-    if (isUserExist) {
-      throw new ConflictException('User with this email already exists.');
+      if (isUserExist) {
+        throw new ConflictException('User with this email already exists.');
+      }
+
+      const userCredential = await FirebaseAuth.createUserWithEmailAndPassword(
+        FirebaseAuth.getAuth(),
+        email,
+        password,
+      );
+
+      const user = this.userRepository.create({
+        name,
+        email,
+      });
+
+      await this.userRepository.save(user);
+
+      return {
+        statusCode: HttpStatus.CREATED,
+        message: 'User registered successfully',
+        acceessToken: await userCredential.user.getIdToken(),
+      };
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        this.firebaseClientService.handleFirebaseError(error);
+      } else {
+        return {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: (error as Error).message,
+        };
+      }
     }
-
-    const userCredential = await FirebaseAuth.createUserWithEmailAndPassword(
-      FirebaseAuth.getAuth(),
-      email,
-      password,
-    );
-
-    const user = this.userRepository.create({
-      name,
-      email,
-    });
-
-    await this.userRepository.save(user);
-
-    return {
-      statusCode: HttpStatus.CREATED,
-      message: 'User registered successfully',
-      acceessToken: await userCredential.user.getIdToken(),
-    };
   }
 
   async loginUser({ email, password }: LoginUserDto) {
-    const isUserExist = await this.userRepository.findOneBy({
-      email,
-    });
+    try {
+      const isUserExist = await this.userRepository.findOneBy({
+        email,
+      });
 
-    if (!isUserExist) {
-      throw new NotFoundException('User with this email does not exist.');
+      if (!isUserExist) {
+        throw new NotFoundException('User with this email does not exist.');
+      }
+
+      const userCredential = await FirebaseAuth.signInWithEmailAndPassword(
+        FirebaseAuth.getAuth(),
+        email,
+        password,
+      );
+
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Sign-in successful',
+        acceessToken: await userCredential.user.getIdToken(),
+      };
+    } catch (error) {
+      if (error instanceof FirebaseError) {
+        this.firebaseClientService.handleFirebaseError(error);
+      } else {
+        return {
+          statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+          message: (error as Error).message,
+        };
+      }
     }
-
-    const userCredential = await FirebaseAuth.signInWithEmailAndPassword(
-      FirebaseAuth.getAuth(),
-      email,
-      password,
-    );
-
-    return {
-      statusCode: HttpStatus.OK,
-      message: 'Sign-in successful',
-      acceessToken: await userCredential.user.getIdToken(),
-    };
   }
 
   findOne(id: number) {
