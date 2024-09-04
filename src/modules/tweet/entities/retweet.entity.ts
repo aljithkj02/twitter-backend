868bc0c83be102a9d1cbe2cb1ec4d6b5486de4ce
aljithkj02 <kwdeathlok@gmail.com>
2024-09-04 10:32:12 +0530

    Add retweet entity

---
+++ src/modules/tweet/entities/retweet.entity.ts
@@ -0,0 +1,23 @@
+import { User } from '@/modules/user/entities/user.entity';
+import {
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Tweet } from '@modules/tweet/entities/tweet.entity';
+
+@Entity('retweets')
+export class Retweet {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.retweets, { onDelete: 'CASCADE' })
+  user: User;
+
+  @ManyToOne(() => Tweet, (tweet) => tweet.retweets, { onDelete: 'CASCADE' })
+  tweet: Tweet;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
